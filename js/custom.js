2500741bce88457035ca0cfd9d224dfab8956ac0
samribe <samribe@outlook.com>
2022-10-16 11:57:23 -0300

    footer/footer-menu/social-icons/sub-footer

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,20 @@
 // instancia jquery e evita conflitos
+// jQuery( function($)) {}
 $(document).ready(function(){
 
     $('.owl-carousel').owlCarousel();
+
+    let titulos = $('h4') // tag
+
+    let itens = $('.featured-item') // class
+
+    let destaques = $('#featured') // id
+
+    console.log(titulos.first());
+
+    // Product config
+
+    $('.featured-item a').addClass('btn btn-dark stretch-link');
+
+    $('.featured-item:first h4').append('<span class="badge bg-secondary">Novo</span>')
 })
